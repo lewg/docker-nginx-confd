38f06866f1e43a44803836ed1db2716033318e03
Lew Goettner <lew@goettner.net>
2014-11-20 22:38:26 -0500

    Remove registry, update etcd container

--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@ ETCD_HOST=172.17.8.101:4002
 etcdctl --peers $ETCD_HOST set /nginx/upstreams/app1/endpoints/server1 backend_1
 etcdctl --peers $ETCD_HOST set /nginx/upstreams/app2/endpoints/server1 backend_1
 
-etcdctl --peers $ETCD_HOST set /nginx/hosts/localhost/upstream app2
+etcdctl --peers $ETCD_HOST set /nginx/hosts/docker.local/upstream app2
 
-etcdctl --peers $ETCD_HOST set /nginx/registry/hostname docker.local
-etcdctl --peers $ETCD_HOST set /nginx/registry/upstream app1
+# etcdctl --peers $ETCD_HOST set /nginx/registry/hostname docker.local
+# etcdctl --peers $ETCD_HOST set /nginx/registry/upstream app1
 
-etcdctl --peers $ETCD_HOST set /nginx/registry/users/test 'test:$apr1$NUgP1viV$xpr/B.wAZK70Vxh3nnWM3.'
+# etcdctl --peers $ETCD_HOST set /nginx/registry/users/test 'test:$apr1$NUgP1viV$xpr/B.wAZK70Vxh3nnWM3.'
